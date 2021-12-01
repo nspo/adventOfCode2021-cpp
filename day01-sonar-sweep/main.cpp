d8cbd3b561972679c8fc06d4428925d0e81709e7
Nicolai Spohrer <nicolai@xeve.de>
2021-12-01 20:32:04 +0100

    Make day01 solution a bit more generic

--- day01-sonar-sweep/main.cpp
+++ day01-sonar-sweep/main.cpp
@@ -1,48 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cassert>
 #include <optional>
+
+int countIncreases(const std::vector<int> &values, const int window_size)
+{
+    assert(window_size >= 1);
+    assert(values.size() <= static_cast<size_t>(std::numeric_limits<int>::max()));
+
+    // accumulate initial sum
+    int prev_sum = 0;
+    for (int i = 0; i < window_size; ++i)
+    {
+        prev_sum += values[i];
+    }
+
+    // actually start to compare values
+    int increases = 0;
+    for (int i = window_size; i < static_cast<int>(values.size()); ++i)
+    {
+        const int old_elem = values[i - window_size]; // will not be in new sum anymore
+        const int new_elem = values[i];               // will only be in new sum
+        const int new_sum = prev_sum - old_elem + new_elem;
+        if (new_sum > prev_sum)
+        {
+            ++increases;
+        }
+        prev_sum = new_sum;
+    }
+
+    return increases;
+}
 
 int main()
 {
-    std::cout << "--- Part 1 ---\n";
     const auto filename = "input1.txt";
     std::ifstream ifs(filename);
     if (!ifs) std::terminate();
 
-    // read data into vector as it's needed again for part 2
-    std::vector<int> input_values;
+    std::vector<int> values;
     {
         int curr_value{};
         while (ifs >> curr_value)
         {
-            input_values.push_back(curr_value);
+            values.push_back(curr_value);
         }
     }
 
-    std::optional<int> prev_value{};
-    int increases{0}; ///< number of increases relative to previous value
-    for (const int curr_value : input_values) 
-    {
-        if (prev_value.has_value() && prev_value.value() < curr_value) {
-            // increase detected
-            ++increases;
-        }
-        prev_value = curr_value;
-    }
-
-    std::cout << increases << " increases detected in 1-value windows in " << filename << "\n";
+    std::cout << "--- Part 1 ---\n";
+    std::cout << countIncreases(values, 1) << " increases detected in 1-value windows in " << filename << "\n";
 
     std::cout << "--- Part 2 ---\n";
-    increases = 0; // reset counter
-    for (size_t i=3; i < input_values.size(); ++i)
-    {
-        // could calculate window sum without reading N values each step, but for N=3 it doesn't seem worth it
-        const int prev_sum = input_values[i-1] + input_values[i-2] + input_values[i-3];
-        const int curr_sum = prev_sum - input_values[i-3] + input_values[i];
-        if (curr_sum > prev_sum) {
-            ++increases;
-        }
-    }
-    std::cout << increases << " increases detected in 3-value windows in " << filename << "\n";
+    std::cout << countIncreases(values, 3) << " increases detected in 3-value windows in " << filename << "\n";
 }

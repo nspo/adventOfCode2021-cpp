f3b45c31c596cd410b849d7d3d9a825ba5ce5745
Nicolai Spohrer <nicolai@xeve.de>
2021-12-02 22:50:21 +0100

    Clean up a bit and use Google's clang-format style

--- day02-dive/main.cpp
+++ day02-dive/main.cpp
@@ -1,42 +1,48 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
+#include <iostream>
 #include <regex>
+#include <vector>
 
-class Submarine
-{
-public:
-    void followCommand(const std::string &raw_command)
-    {
+class Submarine {
+   public:
+    void followCommands(const std::vector<std::string> &raw_commands) {
+        for (const auto &cmd : raw_commands) {
+            followCommand(cmd);
+        }
+    }
+
+    virtual void followCommand(const std::string &raw_command) {
         const auto [cmd, units] = Submarine::splitCommand(raw_command);
 
-        if (cmd == "forward")
-        {
+        if (cmd == "forward") {
             m_pos_horiz += units;
-        }
-        else if (cmd == "down")
-        {
+        } else if (cmd == "down") {
             m_depth += units;
-        }
-        else
-        {
+        } else {
             // up
             m_depth -= units;
         }
     }
 
+    void printLocation() const {
+        std::cout << "Horizontal position: " << m_pos_horiz
+                  << ", depth: " << m_depth
+                  << ", product of both: " << m_pos_horiz * m_depth << "\n";
+    }
+
     int getPosHoriz() const { return m_pos_horiz; }
     int getDepth() const { return m_depth; }
 
-protected:
+    virtual ~Submarine() = default;
+
+   protected:
     // split command into command string and units
-    static std::pair<std::string, int> splitCommand(const std::string &raw_command)
-    {
+    static std::pair<std::string, int> splitCommand(
+        const std::string &raw_command) {
         // C++ regex actually has bad perf, but should be alright for this
         const std::regex pattern{"^(forward|down|up) ([0-9]+)$"};
         std::smatch match;
-        if (!std::regex_match(raw_command, match, pattern))
-        {
+        if (!std::regex_match(raw_command, match, pattern)) {
             throw std::invalid_argument("Invalid raw command: " + raw_command);
         }
 
@@ -47,45 +53,35 @@ protected:
     int m_depth{0};
 };
 
-class Submarine2 : public Submarine
-{
-public:
-    void followCommand(const std::string &raw_command)
-    {
+class Submarine2 : public Submarine {
+   public:
+    void followCommand(const std::string &raw_command) override {
         const auto [cmd, units] = Submarine::splitCommand(raw_command);
 
-        if (cmd == "down")
-        {
+        if (cmd == "down") {
             m_aim += units;
-        }
-        else if (cmd == "up")
-        {
+        } else if (cmd == "up") {
             m_aim -= units;
-        }
-        else
-        {
+        } else {
             // forward
             m_pos_horiz += units;
             m_depth += m_aim * units;
         }
     }
 
-protected:
+   protected:
     int m_aim{0};
 };
 
-int main()
-{
+int main() {
     const auto filename = "input1.txt";
     std::ifstream ifs(filename);
-    if (!ifs)
-        std::terminate();
+    if (!ifs) std::terminate();
 
     std::vector<std::string> commands;
     {
         std::string line{};
-        while (std::getline(ifs, line))
-        {
+        while (std::getline(ifs, line)) {
             commands.emplace_back(std::move(line));
         }
     }
@@ -93,23 +89,14 @@ int main()
     {
         std::cout << "--- Part 1 ---\n";
         Submarine sub;
-        for (const auto &command : commands)
-        {
-            sub.followCommand(command);
-        }
-
-        const int answer1 = sub.getPosHoriz() * sub.getDepth();
-        std::cout << "Pos horiz: " << sub.getPosHoriz() << ", depth: " << sub.getDepth() << ", product: " << answer1 << "\n";
+        sub.followCommands(commands);
+        sub.printLocation();
     }
 
     {
         std::cout << "--- Part 2 ---\n";
         Submarine2 sub2;
-        for (const auto &command : commands)
-        {
-            sub2.followCommand(command);
-        }
-        const int answer2 = sub2.getPosHoriz() * sub2.getDepth();
-        std::cout << "Pos horiz: " << sub2.getPosHoriz() << ", depth: " << sub2.getDepth() << ", product: " << answer2 << "\n";
+        sub2.followCommands(commands);
+        sub2.printLocation();
     }
 }
